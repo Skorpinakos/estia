f60677039f5b21c40a0f22e6457ad1fa2fd7cba6
Ioannis Tsampras <gtsambras@gmail.com>
2024-03-12 17:04:37 +0200

    github pages mod

--- app.py
+++ app.py
@@ -5,32 +5,32 @@ app = Flask(__name__)
 # Serve the main page
 @app.route('/')
 def index():
-    return send_from_directory('.', 'front-end/index.html')
+    return send_from_directory('.', 'index.html')
 
 # Serve CSS files
 @app.route('/styles.css')
 def styles():
-    return send_from_directory('.', 'front-end/styles.css')
+    return send_from_directory('.', 'styles.css')
 
 # Serve JavaScript files
 @app.route('/script.js')
 def script():
-    return send_from_directory('.', 'front-end/script.js')
+    return send_from_directory('.', 'script.js')
 
 # Serve images dynamically
 @app.route('/<filename>.JPG')
 def serve_image(filename):
-    return send_from_directory('.', f'front-end/{filename}.JPG')
+    return send_from_directory('.', f'{filename}.JPG')
 
 # Serve images dynamically
 @app.route('/<filename>.svg')
 def serve_svg(filename):
-    return send_from_directory('.', f'front-end/{filename}.svg')
+    return send_from_directory('.', f'{filename}.svg')
 
 # Serve images dynamically
 @app.route('/<filename>.png')
 def serve_png(filename):
-    return send_from_directory('.', f'front-end/{filename}.png')
+    return send_from_directory('.', f'{filename}.png')
 
 if __name__ == '__main__':
     app.run(debug=True, port=1100, host='0.0.0.0')
