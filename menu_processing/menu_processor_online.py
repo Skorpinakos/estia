2ea99091680e95aedf4c4a3ddcb440426c9a268c
Ioannis Tsampras <gtsambras@gmail.com>
2024-03-17 19:38:00 +0200

    got menus

---
+++ menu_processing/menu_processor_online.py
@@ -0,0 +1,20 @@
+def get_current_menus():
+    import requests
+    from bs4 import BeautifulSoup
+
+    url = "https://my.upatras.gr"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    paragraph_texts = [p.text for p in soup.find_all('p')] #this check is kinda good but it could get better
+    food_texts=[]
+    for text in paragraph_texts:
+        if "Πιάτο" in text or "καφές" in text: #this check is kinda good but it could get better
+            food_texts.append(text)
+
+    menus=food_texts[0:3]  ### each menu appears 3 times so we take the first 3
+    return menus
+
+
+
+print(get_current_menus())
