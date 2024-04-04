dba258ea8f6995c878978664771046b6fac35f2f
Skorpinakos <ioannis.tsampras@upnet.gr>
2024-04-04 14:36:25 +0300

    fix?

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,9 @@
 const CACHE_NAME = 'sense-campus-patras-v1';
-const urlsToCache = [/*
+const urlsToCache = [
   '/index.html',
   '/styles.css', 
   '/script.js',  
-  '/manifest.json',*/
-  '/media/icons/icon_64.png',
-  '/media/icons/icon_192.png',
-  '/media/icons/icon_256.png',
+  '/manifest.json',
   '/media/icons/icon_512.png',
   '/data.js',
   'media/clock.svg',
