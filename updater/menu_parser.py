523971e7a3f6f0ce4751c2ea9896e90e971da3ad
Skorpinakos <ioannis.tsampras@upnet.gr>
2024-04-26 16:21:49 +0300

    added br for big words

--- updater/menu_parser.py
+++ updater/menu_parser.py
@@ -20,8 +20,8 @@ def process_string(input_string):
     safe_string=[]
     for i in updated_string.split(" "):
         if len(i)>=13 and not ("-" in i):
-            safe_string.append(i[0:int(len(i)/2)]+"‐"+" ")
-            safe_string.append(i[int(len(i)/2):]+" ")
+            safe_string.append(i[0:int(3*len(i)/4)]+"‐"+" "+"<br>")
+            safe_string.append(i[int(len(i)/4):]+" ")
             print(i)
         else:
             safe_string.append(i+" ")
